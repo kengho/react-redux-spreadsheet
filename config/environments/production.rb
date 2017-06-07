3e02eabdee0b75844a329ca40573d921bb4c2285
Alexander Morozov <ntcomp12@gmail.com>
2017-06-07 15:44:22 +0300

    removed legacy suffix from cookies' names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "spreadsheet-es6_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "spreadsheet-_#{Rails.env}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
