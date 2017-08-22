f2c1980c96ff055cd1d992681b70108798807263
Alexander Morozov <ntcomp12@gmail.com>
2017-08-22 21:47:19 +0300

    fixed keydown propagation issue (keydown on document fired always first, ignoring propagation settings)

--- client/src/containers/Spreadsheet.jsx
+++ client/src/containers/Spreadsheet.jsx
@@ -35,7 +35,7 @@ class Spreadsheet extends React.Component {
   constructor(props) {
     super(props);
 
-    this.documentKeyDownHandler = this.documentKeyDownHandler.bind(this);
+    this.windowKeyDownHandler = this.windowKeyDownHandler.bind(this);
 
     this.fictiveRows = [`r${uuid()}`, `r${uuid()}`];
     this.fictiveColumns = [`c${uuid()}`, `c${uuid()}`];
@@ -55,7 +55,13 @@ class Spreadsheet extends React.Component {
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.documentKeyDownHandler); // eslint-disable-line no-undef
+    // TODO: add removeEventListener on componentWillUnmount.
+    // NOTE: don't use document.addEventListener here, it fires
+    //   whenever stopImmediatePropagation is set in childs or not.
+    //   Wrapping addEventListener in setTimeout helps,
+    //   but it looks like ugly hack.
+    // TODO: figure out why this happens.
+    window.addEventListener('keydown', this.windowKeyDownHandler); // eslint-disable-line no-undef
 
     // Don't fetch data from server in tests.
     if (process.env.NODE_ENV === 'test') {
@@ -90,7 +96,7 @@ class Spreadsheet extends React.Component {
     return true;
   }
 
-  documentKeyDownHandler(evt) {
+  windowKeyDownHandler(evt) {
     const action = findKeyAction(evt, [
       {
         condition: () => (evt.key.length === 1 || evt.key === 'Enter' || evt.key === 'F2'),
