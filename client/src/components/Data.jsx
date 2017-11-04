kengho/react-redux-spreadsheet

commit 270ee1c8f46c12896d129e1651f93bcaca2795a8

removed ids from DataCells by upgrading saveEditingCellValueOnPointerMove middleware using detachment (not causing component update) props

client/src/components/Data.jsx
@@ -28,6 +28,7 @@ class Data extends React.PureComponent {
     this.clickHandler = this.clickHandler.bind(this);
     this.doubleClickHandler = this.doubleClickHandler.bind(this);
     this.keyDownHandler = this.keyDownHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
   }
 
   componentDidMount() {
@@ -83,6 +84,15 @@ class Data extends React.PureComponent {
   onHeightChangeHandler(textareaInputEl) {
     const heightNumber = cssToNumber(textareaInputEl.style.height);
     textareaInputEl.style.height = numberToCss(heightNumber + 1);
+  }
+
+  onChangeHandler(evt) {
+    if (!evt) {
+      return;
+    }
+
+    const value = evt.target.value;
+    this.props.actions.detachmentsSetCurrentCellValue(value);
   }
 
   keyDownHandler(evt) {
@@ -186,6 +196,7 @@ class Data extends React.PureComponent {
           inputRef={(c) => { this.textareaInputEl = c; }}
           key={JSON.stringify({ cellId, value, isPointed, isEditing, isSelectingOnFocus })}
           maxWidth="512px"
+          onChange={this.onChangeHandler}
           onFocus={(evt) => this.onFocusHandler(evt, isSelectingOnFocus)}
           onHeightChange={() => this.onHeightChangeHandler(this.textareaInputEl)}
           onKeyDown={this.keyDownHandler}
@@ -206,10 +217,10 @@ class Data extends React.PureComponent {
       );
     }
 
+    // id for saveEditingCellValueOnPointerMove().
     return (
       <div
         className={textareaWrapperClassnames.join(' ')}
-        id={cellId}
         onClick={(evt) => this.clickHandler(evt, cellId)}
         onDoubleClick={(evt) => this.doubleClickHandler(evt, cellId)}
       >

client/src/actions/detachments.js
@@ -1,0 +1,9 @@
+// Changing this state branch props doesn't affects on Spreadsheet re-render via connect().
+// Such changes could be handeled only in middleware.
+
+export function detachmentsSetCurrentCellValue(currentCellValue) { // eslint-disable-line import/prefer-default-export
+  return {
+    type: 'DETACHMENTS/SET_CURRENT_CELL_VALUE',
+    currentCellValue,
+  };
+}

client/src/containers/Spreadsheet.jsx
@@ -1,5 +1,4 @@
 import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import { LinearProgress } from 'material-ui/Progress';
 import React from 'react';
 
@@ -9,6 +8,7 @@ import {
   getRowId,
 } from '../core';
 import './Spreadsheet.css';
+import * as DetachmentsActions from '../actions/detachments';
 import * as LandingActions from '../actions/landing'; // landingSetMessages()
 import * as MetaActions from '../actions/meta';
 import * as RequestsActions from '../actions/requests';
@@ -21,6 +21,7 @@ import findKeyAction from '../lib/findKeyAction';
 import getRootPath from '../lib/getRootPath';
 import isScrolledIntoView from '../lib/isScrolledIntoView';
 import Row from '../components/Row';
+import connectWithSkippingProps from '../lib/connectWithSkippingProps';
 import shiftScrollbar from '../lib/shiftScrollbar';
 import TableMenu from '../components/TableMenu';
 
@@ -36,17 +37,19 @@ const mapStateToProps = (state) => {
   }
 
   return {
+    canRedo: state.get('table').future.length > 0,
+    canUndo: state.get('table').past.length > 1, // omitting TABLE/SET_TABLE_FROM_JSON
+    detachments: state.get('detachments'),
     requests: state.get('requests'),
     table,
     ui: state.get('ui'),
-    canRedo: state.get('table').future.length > 0,
-    canUndo: state.get('table').past.length > 1, // omitting TABLE/SET_TABLE_FROM_JSON
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   actions: {
     ...bindActionCreators({
+      ...DetachmentsActions,
       ...LandingActions,
       ...MetaActions,
       ...RequestsActions,
@@ -420,4 +423,10 @@ class Spreadsheet extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Spreadsheet);
+const detachmentsProps = ['detachments'];
+
+export default connectWithSkippingProps(
+  mapStateToProps,
+  mapDispatchToProps,
+  detachmentsProps
+)(Spreadsheet);

client/src/lib/connectWithSkippingProps.js
@@ -1,0 +1,26 @@
+import { connect } from 'react-redux';
+import shallowEqualWithSkippingProps from '../lib/shallowEqualWithSkippingProps';
+
+const connectWithSkippingProps = (
+  mapStateToProps,
+  mapDispatchToProps,
+  skippingProps = []
+) => {
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    undefined,
+    {
+      areStatePropsEqual: (nextStateProps, stateProps) => {
+        return shallowEqualWithSkippingProps(
+          nextStateProps,
+          stateProps,
+          skippingProps
+        );
+      },
+    }
+  );
+};
+
+export default connectWithSkippingProps;
+ 

client/src/lib/shallowEqualWithSkippingProps.js
@@ -1,0 +1,44 @@
+// Modified shallowequal from dashed/shallowequal
+// https://github.com/dashed/shallowequal/blob/master/index.js
+// with skippingProps param
+// (also some redundant for this app checks are skipped).
+//
+// TODO: test.
+const shallowEqualWithSkippingProps = (objA, objB, skippingProps = []) => {
+  if (objA === objB) {
+    return true;
+  }
+
+  var keysA = Object.keys(objA);
+  var keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+
+  // Test for A's keys different from B.
+  for (var idx = 0; idx < keysA.length; idx++) {
+    var key = keysA[idx];
+
+    if (skippingProps.indexOf(key) !== -1) {
+      continue;
+    }
+
+    if (!bHasOwnProperty(key)) {
+      return false;
+    }
+
+    var valueA = objA[key];
+    var valueB = objB[key];
+
+    if (valueA !== valueB) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export default shallowEqualWithSkippingProps;

client/src/reducers/detachments.js
@@ -1,0 +1,18 @@
+import { fromJS } from 'immutable';
+
+const defaultState = fromJS({
+  currentCellValue: null,
+});
+
+export default function meta(state = defaultState, action) {
+  switch (action.type) {
+    case 'DETACHMENTS/SET_CURRENT_CELL_VALUE':
+      return state.set(
+        'currentCellValue',
+        action.currentCellValue
+      );
+
+    default:
+      return state;
+  }
+}

client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux-immutable';
 import { routerReducer } from 'react-router-redux';
 import undoable from 'redux-undo';
 
+import detachments from './detachments';
 import landing from './landing';
 import meta from './meta';
 import requests from './requests';
@@ -9,6 +10,7 @@ import table from './table';
 import ui from './ui';
 
 const rootReducer = combineReducers({
+  detachments,
   landing,
   meta,
   requests,

client/src/store/middleware/saveEditingCellValueOnPointerMove.js
@@ -1,63 +1,47 @@
 import { tableSetProp } from '../../actions/table';
+import { detachmentsSetCurrentCellValue } from '../../actions/detachments';
 
 const saveEditingCellValueOnPointerMove = store => next => action => { // eslint-disable-line consistent-return
-  // HACK: saving previously edited cells' value.
+  // Saving previously edited cells' value via detachments.
   //   It should be done in middleware because DataCell doesn't know
   //   about previous pointer if user clicks on another DataCell.
   //   If user presses keydown - it knows, but for our convenience DataCell's
-  //   keyDownHandler also doesn't changes cells' values until this hack is present.
-  //
-  //   Possible ways of getting rid of this:
-  //   1) Store current cells' value in store.
-  //   Issue: state changing fires a lot of events, potentially slowing down the app.
-  //   2) Store current cells' value somewhere else.
-  //   Issue: not any better than current implementation.
-  //   3) Pass previous pointer to every DataCell.
-  //   Issue: to complex and, again, slow.
+  //   keyDownHandler also doesn't changes cells' values until this middleware is present.
   //
   //   Only TABLE/SET_POINTER or TABLE/MOVE_POINTER may trigger value changing.
-  if (action.type === 'TABLE/SET_POINTER' || action.type === 'TABLE/MOVE_POINTER') {
-    const previousTable = store.getState().get('table').present;
-    const previousPointerCellId = previousTable.getIn(['session', 'pointer', 'cellId']);
+  if (!(action.type === 'TABLE/SET_POINTER' || action.type === 'TABLE/MOVE_POINTER')) {
+    return next(action);
+  }
 
-    if (previousPointerCellId) {
-      let previousPointerCellNextValue;
-      const probablyPreviousPointerCell = document
-        .getElementById(previousPointerCellId);
-      if (probablyPreviousPointerCell) {
-        const previousPointerCellTextarea = probablyPreviousPointerCell
-          .querySelector('textarea');
-        if (previousPointerCellTextarea) {
-          previousPointerCellNextValue = previousPointerCellTextarea.value;
-        }
-      }
+  const table = store.getState().get('table').present;
+  const pointerCellId = table.getIn(['session', 'pointer', 'cellId']);
+  if (!pointerCellId) {
+    return next(action);
+  }
 
-      const previousPointerCell = previousTable
-        .getIn(['data', 'cells'])
-        .get(previousPointerCellId);
-      let previousPointerCellPreviousValue;
-      if (previousPointerCell) {
-        previousPointerCellPreviousValue = previousPointerCell.get('value');
-      }
+  const pointedCellNextValue = store.getState().getIn(['detachments', 'currentCellValue']);
+  if (!pointedCellNextValue) {
+    return next(action);
+  }
 
-      // Normalizing values (empty to undefined).
-      if (previousPointerCellPreviousValue === '') {
-        previousPointerCellPreviousValue = undefined;
-      }
-      if (previousPointerCellNextValue === '') {
-        previousPointerCellNextValue = undefined;
-      }
+  let pointedCellPreviousValue;
+  const pointedCell = table.getIn(['data', 'cells', pointerCellId]);
+  if (pointedCell) {
+    pointedCellPreviousValue = pointedCell.get('value');
+  }
 
-      if (previousPointerCellNextValue !== previousPointerCellPreviousValue) {
-        store.dispatch(
-          tableSetProp(
-            previousPointerCellId,
-            'value',
-            previousPointerCellNextValue
-          )
-        );
-      }
-    }
+  if (pointedCellNextValue !== pointedCellPreviousValue) {
+    store.dispatch(
+      tableSetProp(
+        pointerCellId,
+        'value',
+        pointedCellNextValue
+      )
+    );
+
+    // Clear currentCellValue so this middleware won't run again after
+    // after several consecutive pointer changes without any text typing.
+    store.dispatch(detachmentsSetCurrentCellValue(null));
   }
 
   return next(action);

commit 2413bcf7fd871e5695ce00808e212c62e70bc55c

fixed broken esc handler while editing cell (was broken since 270ee1c8f46c12896d129e1651f93bcaca2795a8)

client/src/components/Data.jsx
@@ -168,7 +168,8 @@ class Data extends React.PureComponent {
       {
         key: 'Escape',
         action: () => {
-          this.textareaInputEl.value = this.props.value;
+          const previousValue = this.props.value;
+          this.props.actions.detachmentsSetCurrentCellValue(previousValue);
           this.props.actions.tableSetPointer({ modifiers: {} });
         },
       },