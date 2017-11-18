e73e596661889ee43c7aeeb48fe79424ba864d60
Alexander Morozov <ntcomp12@gmail.com>
2017-11-18 13:43:59 +0300

    added guard clause

--- client/src/store/middleware/expandTableOnPointerMove.js
+++ client/src/store/middleware/expandTableOnPointerMove.js
@@ -8,20 +8,22 @@ import { tableExpand } from '../../actions/table';
 const expandTableOnPointerMove = store => next => action => { // eslint-disable-line consistent-return
   const nextAction = next(action);
 
-  if (action.type === 'TABLE/MOVE_POINTER') {
-    const nextTable = store.getState().get('table').present;
-    const nextRows = nextTable.getIn(['data', 'rows']);
-    const nextColumns = nextTable.getIn(['data', 'columns']);
-    const nextPointerCellId = nextTable.getIn(['session', 'pointer', 'cellId']);
-    const nextPointerRowId = getRowId(nextPointerCellId);
-    const nextPointerColumnId = getColumnId(nextPointerCellId);
+  if (action.type !== 'TABLE/MOVE_POINTER') {
+    return nextAction;
+  }
 
-    if (nextRows.findIndex((row) => row.get('id') === nextPointerRowId) === -1) {
-      // slice deletes 'r' and 'c' prefixes from ids, because tableExpand() adds them by itself.
-      store.dispatch(tableExpand(nextRows.size, 'ROW', nextPointerRowId.slice('r'.length)));
-    } else if (nextColumns.findIndex((column) => column.get('id') === nextPointerColumnId) === -1) {
-      store.dispatch(tableExpand(nextColumns.size, 'COLUMN', nextPointerColumnId.slice('c'.length)));
-    }
+  const nextTable = store.getState().get('table').present;
+  const nextRows = nextTable.getIn(['data', 'rows']);
+  const nextColumns = nextTable.getIn(['data', 'columns']);
+  const nextPointerCellId = nextTable.getIn(['session', 'pointer', 'cellId']);
+  const nextPointerRowId = getRowId(nextPointerCellId);
+  const nextPointerColumnId = getColumnId(nextPointerCellId);
+
+  if (nextRows.findIndex((row) => row.get('id') === nextPointerRowId) === -1) {
+    // slice deletes 'r' and 'c' prefixes from ids, because tableExpand() adds them by itself.
+    store.dispatch(tableExpand(nextRows.size, 'ROW', nextPointerRowId.slice('r'.length)));
+  } else if (nextColumns.findIndex((column) => column.get('id') === nextPointerColumnId) === -1) {
+    store.dispatch(tableExpand(nextColumns.size, 'COLUMN', nextPointerColumnId.slice('c'.length)));
   }
 
   return nextAction;
