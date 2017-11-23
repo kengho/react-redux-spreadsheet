290078300d02fc8f4e20edcb5daa549bff1e31cf
Alexander Morozov <ntcomp12@gmail.com>
2017-11-23 22:49:35 +0300

    fixed broken copyToRealClipboardOnSetClipboard

--- client/src/store/middleware/copyToRealClipboardOnSetClipboard.js
+++ client/src/store/middleware/copyToRealClipboardOnSetClipboard.js
@@ -5,11 +5,10 @@ import * as ActionTypes from '../../actionTypes';
 const copyToRealClipboardOnSetClipboard = store => next => action => { // eslint-disable-line consistent-returOnSetClipboardn
   if (action.type === ActionTypes.SET_CLIPBOARD) {
     // TODO: handle many selected cells (also see containers/Spreadsheet.jsx).
-    // TODO: fix (cellId is Array here fsr).
-    const cellId = Object.keys(action.clipboard.cells);
+    const cellId = Object.keys(action.clipboard.cells)[0];
     const cell = action.clipboard.cells[cellId];
     if (cell) {
-      copy(cell.value);
+      copy(cell.get('value'));
     }
   }
 
