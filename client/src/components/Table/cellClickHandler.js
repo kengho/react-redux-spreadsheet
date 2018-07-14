6123d7373f165dd46a92116834995b35bd721127
Alexander Morozov <ntcomp12@gmail.com>
2018-07-14 21:27:00 +0300

    optimized cellClickHandler performance and made little adjustments

--- client/src/components/Table/cellClickHandler.js
+++ client/src/components/Table/cellClickHandler.js
@@ -12,14 +12,13 @@ import getCellProps, {
   getCellSize,
 } from '../../lib/getCellProps';
 import * as TableActions from '../../actions/table';
+import * as UiActions from '../../actions/ui';
 import getMousePosition from '../../lib/getMousePosition';
 
 const LEFT_BUTTON = 0;
 const RIGHT_BUTTON = 2;
 
 export default function cellClickHandler({ evt, pointedCell }) {
-  // TODO: PERF: batch all actions here.
-
   const {
     actions,
     table,
@@ -35,6 +34,9 @@ export default function cellClickHandler({ evt, pointedCell }) {
     return;
   }
 
+  // console.time('cellClickHandler');
+  const actionsToBatch = [];
+
   const cellSelector =
     `[data-component-name="${CELL}"]` +
     `[data-row-index="${cellPosition[ROW].index}"]` +
@@ -47,29 +49,31 @@ export default function cellClickHandler({ evt, pointedCell }) {
   const cellPlacement = composeCellProps(cellOffsets, cellSize, cellPosition);
 
   // Selection.
-  // console.time('selection');
   // TODO: add ability to select several boundaries with ctrl.
   if (!currentSelectionVisibility && (evt.type === 'mousedown') && (evt.button === LEFT_BUTTON)) {
-    actions.clearSelection();
-    actions.setCurrentSelectionAnchor({
-      selectionAnchorType: BEGIN,
-      anchor: cellPlacement,
-    });
-    actions.setCurrentSelectionAnchor({
-      selectionAnchorType: END,
-      anchor: cellPlacement,
-    });
-    actions.setCurrentSelectionVisibility(true);
+    actionsToBatch.push(
+      TableActions.clearSelection(),
+      TableActions.setCurrentSelectionAnchor({
+        selectionAnchorType: BEGIN,
+        anchor: cellPlacement,
+      }),
+      TableActions.setCurrentSelectionAnchor({
+        selectionAnchorType: END,
+        anchor: cellPlacement,
+      }),
+      TableActions.setCurrentSelectionVisibility(true),
+    );
   } else if (currentSelectionVisibility && (evt.type === 'mouseover') && (evt.button === LEFT_BUTTON)) {
-    actions.setCurrentSelectionAnchor({
+    actionsToBatch.push(TableActions.setCurrentSelectionAnchor({
       selectionAnchorType: END,
       anchor: cellPlacement,
-    });
+    }));
   } else if (currentSelectionVisibility && (evt.type === 'mouseup') && (evt.button === LEFT_BUTTON)) {
-    actions.fixateCurrentSelection();
-    actions.setCurrentSelectionVisibility(false);
+    actionsToBatch.push(
+      TableActions.fixateCurrentSelection(),
+      TableActions.setCurrentSelectionVisibility(false),
+    );
   }
-  // console.timeEnd('selection');
 
   // Clicks.
   // HACK: in Chrome "click" not fired when user rightclicks,
@@ -78,40 +82,42 @@ export default function cellClickHandler({ evt, pointedCell }) {
   if ((evt.type === 'mousedown') && (evt.button === RIGHT_BUTTON)) {
     const cellPosition = getCellPosition({ evt });
 
-    actions.setMenu({
-      place: CELL,
-      ...composeCellProps(
-        cellPosition,
+    actionsToBatch.push(
+      UiActions.setMenu({
+        place: CELL,
+        ...composeCellProps(
+          cellPosition,
+          {
+            [ROW]: {
+              offset: getMousePosition(evt).page.y,
+            },
+            [COLUMN]: {
+              offset: getMousePosition(evt).page.x,
+            },
+          },
+        ),
+      }),
+      UiActions.openPopup(),
+
+      // TODO: don't set pointer if there are selection (when there is code for selection).
+      TableActions.setPointer(composeCellProps(
+        // NOTE: if we don't wipe lines' sizes, if we set pointer on non-default size cell,
+        //   scroll it out of sight, and then point some empty cell, it's size will change.
         {
           [ROW]: {
-            offset: getMousePosition(evt).page.y,
+            size: null,
           },
           [COLUMN]: {
-            offset: getMousePosition(evt).page.x,
+            size: null,
           },
         },
-      ),
-    })
-    actions.openPopup();
-
-    // TODO: don't set pointer if there are selection (when there is code for selection).
-    actions.setPointer(composeCellProps(
-      // NOTE: if we don't wipe lines' sizes, if we set pointer on non-default size cell,
-      //   scroll it out of sight, and then point some empty cell, it's size will change.
-      {
-        [ROW]: {
-          size: null,
+        {
+          edit: false,
+          selectOnFocus: false,
         },
-        [COLUMN]: {
-          size: null,
-        },
-      },
-      {
-        edit: false,
-        selectOnFocus: false,
-      },
-      cellPosition,
-    ));
+        cellPosition,
+      )),
+    );
 
   // Leftclick.
   // NOTE: we catch "mousedown" instead of "click" for faster UI response.
@@ -126,69 +132,73 @@ export default function cellClickHandler({ evt, pointedCell }) {
       cellPosition[ROW].index === pointer.getIn([ROW, 'index']) &&
       cellPosition[COLUMN].index === pointer.getIn([COLUMN, 'index'])
     );
-    if (userWantsToPointCell && isCellPointed) {
-      return;
-    }
     const isCellEditing = (isCellPointed && pointer.get('edit') === true);
-    if (userWantsToEditCell && isCellEditing) {
-      return;
-    }
 
-    // HACK: saving previously pointed cell's value if it changed.
-    // NOTE: order of actions is important: at first we save value is necessary,
-    //   then we move pointer (otherwise pointed cell changes and value is lost).
-    let pointedCellProps;
-    if (pointedCell) {
-      pointedCellProps = getCellProps(pointedCell);
-    }
-    // HACK: if cell was editing, when scrolled out of sight
-    //   and then scrolled back again, cell.innerText (see getCellValue())
-    //   is '' despite being not empty, so we getting data from store.
-    if (!pointedCellProps || (pointedCellProps && pointedCellProps.value === '')) {
-      const savedPointedCell = table.getIn(['session', 'pointer']);
-      pointedCellProps = savedPointedCell.toJS();
-    }
-    if (pointedCellProps) {
-      const previousPointedCellValue = table.getIn([
-        'layout',
-        ROW,
-        'list',
-        pointedCellProps[ROW].index,
-        'cells',
-        pointedCellProps[COLUMN].index,
-        'value',
-      ], '');
-
-      if (pointedCellProps.value !== previousPointedCellValue) {
-        // NOTE: PERF: without batchActions: ~300ms. With batchActions: ~100ms.
-        // console.time('cellClickHandler');
-        actions.batchActions([
-          TableActions.insertRows(pointedCellProps[ROW]),
-          TableActions.insertColumns(pointedCellProps[COLUMN]),
-          TableActions.setProp({
-            ...pointedCellProps,
-            prop: 'value',
-          }),
-        ]);
-        // console.timeEnd('cellClickHandler');
+    if (
+      !(userWantsToPointCell && isCellPointed) &&
+      !(userWantsToEditCell && isCellEditing)
+    ) {
+      // HACK: saving previously pointed cell's value if it changed.
+      // NOTE: order of actions is important: at first we save value is necessary,
+      //   then we move pointer (otherwise pointed cell changes and value is lost).
+      let pointedCellProps;
+      if (pointedCell) {
+        pointedCellProps = getCellProps(pointedCell);
       }
-    }
 
-    // Main action.
-    actions.setPointer(composeCellProps(
-      {
-        [ROW]: {
-          size: null,
-        },
-        [COLUMN]: {
-          size: null,
-        },
-      },
-      {
-        edit: userWantsToEditCell,
-        selectOnFocus: false,
-      },
-      cellPosition,
-    ));
+      // HACK: if cell was editing, when scrolled out of sight
+      //   and then scrolled back again, cell.innerText (see getCellValue())
+      //   is '' despite being not empty, so we getting data from store.
+      if (!pointedCellProps || (pointedCellProps && pointedCellProps.value === '')) {
+        const savedPointedCell = table.getIn(['session', 'pointer']);
+        pointedCellProps = savedPointedCell.toJS();
+      }
+      if (pointedCellProps) {
+        const previousPointedCellValue = table.getIn([
+          'layout',
+          ROW,
+          'list',
+          pointedCellProps[ROW].index,
+          'cells',
+          pointedCellProps[COLUMN].index,
+          'value',
+        ], '');
+
+        if (pointedCellProps.value !== previousPointedCellValue) {
+          actionsToBatch.push(
+            TableActions.insertRows(pointedCellProps[ROW]),
+            TableActions.insertColumns(pointedCellProps[COLUMN]),
+            TableActions.setProp({
+              ...pointedCellProps,
+              prop: 'value',
+            }),
+          );
+        }
+      }
+
+      // Main action.
+      actionsToBatch.push(TableActions.setPointer(
+        composeCellProps(
+          {
+            [ROW]: {
+              size: null,
+            },
+            [COLUMN]: {
+              size: null,
+            },
+          },
+          {
+            edit: userWantsToEditCell,
+            selectOnFocus: false,
+          },
+          cellPosition,
+        ))
+      );
+    }
   }
+
+  if (actionsToBatch.length > 0) {
+    actions.batchActions(actionsToBatch);
+  }
+  // console.timeEnd('cellClickHandler');
 }
