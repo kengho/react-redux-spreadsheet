5f27e2bfc49598238b7e878775dbe0789cac58a0
Alexander Morozov <ntcomp12@gmail.com>
2018-07-16 12:55:38 +0300

    delete action grouping bacause apparently pasting area using batched action considered single action anyway

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux-immutable';
-import undoable, { groupByActionTypes } from 'redux-undo';
+import undoable from 'redux-undo';
 
 import * as ActionTypes from '../actionTypes';
 import landing from './landing';
@@ -16,7 +16,6 @@ export default combineReducers({
     // NOTE: Dispaching ActionTypes.MERGE_SERVER_STATE should save state to history.
     //  https://github.com/omnidan/redux-undo/issues/157#issuecomment-298245650
     filter: (action) => (action.changesData || action.type === ActionTypes.MERGE_SERVER_STATE),
-    groupBy: groupByActionTypes([ActionTypes.SET_PROP, ActionTypes.SET_CELL]),
 
     // For ActionTypes.UNDO and ActionTypes.REDO in table reducer.
     neverSkipReducer: true,
